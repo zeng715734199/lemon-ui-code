292bf822ed92dfe11c4b2cfb9dbc6eedae2ceb31
zeng715734199 <715734199@qq.com>
2022-02-10 17:59:30 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<div>hi</div>
+<cool/>
+</template>
+
+<script>
+import cool from './components/cool.vue'
+
+export default {
+  name: 'App',
+  components: {
+    cool
+  }
+}
+</script>
