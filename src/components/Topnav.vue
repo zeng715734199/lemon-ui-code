b12053e677e5d0aede98fa053d2ac0bdb83319ba
zeng715734199 <715734199@qq.com>
2022-02-14 19:35:33 +0800

    让 Topnav是响应式的

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleAside">LOGO</div>
+    <div class="logo" @click="toggleMenu">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside"></span>
   </div>
 </template>
 <script lang="ts">
@@ -13,10 +14,10 @@ import {inject, Ref} from 'vue';
 export default {
   setup() {
     const asideVisible = inject<Ref<boolean>>('asideVisible');
-    const toggleAside = () => {
+    const toggleMenu = () => {
       asideVisible.value = !asideVisible.value
     }
-    return {toggleAside}
+    return {toggleMenu}
   }
 };
 </script>
@@ -27,7 +28,8 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
-
+  justify-content: center;
+  align-items: center;
   > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -41,6 +43,10 @@ export default {
     > li {
       margin: 0 1em;
     }
+  }
+  @media (max-width: 500px) {
+    > .menu{display: none;}
+    > .logo{margin: 0 auto}
   }
 }
 </style>
