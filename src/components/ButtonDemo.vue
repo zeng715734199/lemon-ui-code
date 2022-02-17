ce7fd44acaa4a68cfca7c65b2035b85954bc5ecf
zeng715734199 <715734199@qq.com>
2022-02-17 21:37:59 +0800

    创建Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-   Button组件内容
+   <Button>你好阿</Button>
   </div>
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue';
+export default {
+  components: {Button}
+}
+</script>
