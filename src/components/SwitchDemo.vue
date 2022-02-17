f5364e8dff241e4547597cc707813cc206a73f0f
zeng715734199 <715734199@qq.com>
2022-02-17 21:33:13 +0800

    美化按钮样式

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Switch  v-model:value="x" />
+    <Switch  v-model:value="bool" />
   </div>
 </template>
 <script>
@@ -10,8 +10,8 @@ import {ref} from 'vue'
 export default {
   components: {Switch},
   setup() {
-    const x = ref(false)
-    return {x}
+    const bool = ref(false)
+    return {bool}
   }
 }
 </script>
