f007f987202a1c30cdee56718b5886acbf7a6ff7
zeng715734199 <715734199@qq.com>
2022-02-18 21:16:03 +0800

    支持theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -52,5 +52,25 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+
+  &.lemon-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover, &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.lemon-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover, &:focus {
+      background: darken(white, 5%);
+    }
+  }
 }
 </style>
