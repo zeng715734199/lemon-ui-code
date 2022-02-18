8da5abfcc70553bb6400d38e399226b0717dfe8e
zeng715734199 <715734199@qq.com>
2022-02-18 17:13:01 +0800

    添加theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,6 +1,11 @@
 <template>
+  <div>Button 示例</div>
+  <h1>示例1</h1>
   <div>
-   <Button>你好阿</Button>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
   </div>
 </template>
 
