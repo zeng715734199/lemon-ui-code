67602172a8e7d3eb5d52a665a30d39402b5e9e8b
zeng715734199 <715734199@qq.com>
2022-02-19 14:55:42 +0800

    基本完善Button组件

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,16 +1,46 @@
 <template>
-  <button class="lemon-button" :class="`lemon-theme-${theme}`">
+  <button class="lemon-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="lemon-loadingIndicator"></span>
     <slot/>
   </button>
 </template>
 
 <script lang="ts">
+import {computed} from 'vue';
+
 export default {
   props: {
     theme: {
       type: String,
       default: 'button'
+    },
+    size: {
+      type: String,
+      default: 'normal'
+    },
+    level: {
+      type: String,
+      default: 'normal'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
     }
+  },
+  setup(props) {
+    const {theme, size, level} = props;
+    const classes = computed(() => {
+      return {
+        [`lemon-theme-${theme}`]: theme,
+        [`lemon-size-${size}`]: size,
+        [`lemon-level-${level}`]: level
+      };
+    });
+    return {classes};
   }
 };
 </script>
@@ -21,6 +51,8 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+$grey: grey;
 .lemon-button {
   box-sizing: border-box;
   height: $h;
@@ -35,6 +67,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
@@ -72,5 +105,104 @@ $radius: 4px;
       background: darken(white, 5%);
     }
   }
+
+  &.lemon-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+
+  &.lemon-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+
+  &.lemon-theme-button {
+    &.lemon-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+
+    &.lemon-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.lemon-theme-link {
+    &.lemon-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.lemon-theme-text {
+    &.lemon-level-main {
+      color: $blue;
+
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+
+    &.lemon-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.lemon-theme-button{
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover{
+        border-color: $grey;
+      }
+    }
+  }
+  &.lemon-theme-link, &.lemon-theme-text {
+    &[disabled]{
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
+  > .lemon-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: lemon-spin 1s infinite linear;
+  }
+}
+@keyframes lemon-spin {
+  0%{transform: rotate(0deg)}
+  100%{transform: rotate(360deg)}
 }
 </style>
