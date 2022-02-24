e8fc0dbb1010d7f2605443a9924314a026717dc6
zeng715734199 <715734199@qq.com>
2022-02-25 00:04:01 +0800

    美化首页，添加切换按钮的样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -6,16 +6,23 @@
       </svg>
     </div>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+      <li>
+        <router-link to="/doc">文档</router-link>
+      </li>
     </ul>
-    <span class="toggleAside" @click="toggleMenu"></span>
+    <svg v-if="toggleMenuButtonVisible" class="toggleAside" @click="toggleMenu">
+      <use xlink:href="#icon-menu"></use>
+    </svg>
   </div>
 </template>
 <script lang="ts">
 import {inject, Ref} from 'vue';
 
 export default {
+  props: {
+    toggleMenuButtonVisible: Boolean,
+    default: false
+  },
   setup() {
     const asideVisible = inject<Ref<boolean>>('asideVisible');
     const toggleMenu = () => {
@@ -38,12 +45,14 @@ $color: #548e15;
   justify-content: center;
   align-items: center;
   color: $color;
+
   > .logo {
     max-width: 6em;
     margin-right: auto;
+
     > svg {
-      height: 64px;
-      width: 64px;
+      height: 32px;
+      width: 32px;
     }
   }
 
@@ -58,14 +67,14 @@ $color: #548e15;
   }
 
   > .toggleAside {
-    width: 24px;
-    height: 24px;
-    background: red;
+    width: 32px;
+    height: 32px;
     position: absolute;
     left: 16px;
     top: 50%;
     transform: translateY(-50%);
     display: none;
+    background: fade-out(black, 0.9);
   }
 
   @media (max-width: 500px) {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,15 +12,29 @@
       </div>
     </div>
     <div class="features">
-      <svg class="icon">
-        <use xlink:href="#icon-Vue"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-ts"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-light"></use>
-      </svg>
+      <ul>
+        <li>
+          <svg>
+            <use xlink:href="#icon-Vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>使用了 Vue 3 Composition API</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 TypeScript </h3>
+          <p>源代码采用 TypeScript 书写（非严格检查）</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-light"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>每个组件的源代码都极其简洁</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -37,10 +51,51 @@ $border-radius: 5px;
 $color: #548e15;
 .topnavAndBanner {
   background: linear-gradient(315deg, rgba(176, 236, 211, 1) 0%, rgba(251, 237, 149, 1) 100%);
+  clip-path: ellipse(80% 60% at 50% 40%);
 }
-.features > svg{
-  height: 64px;
-  width: 64px;
+
+.features {
+  margin: 64px auto;
+  width: 400px;
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+
+  > ul {
+    display: flex;
+    flex-wrap: wrap;
+
+    > li {
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+      "icon title"
+      "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+
+      > svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+
+      > h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+
+      > p {
+        grid-area: text;
+      }
+    }
+  }
 }
 
 .banner {
