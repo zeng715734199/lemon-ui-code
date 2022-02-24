0a56a0bad8c70693943643679c5cf54e812ce3a5
zeng715734199 <715734199@qq.com>
2022-02-24 16:56:39 +0800

    动态设置div的宽度、位置

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="lemon-tabs">
-    <div class="lemon-tabs-nav">
+    <div class="lemon-tabs-nav" ref="container">
       <div class="lemon-tabs-nav-item"
            :class="{selected: t === selected}"
            @click="select(t)"
-           v-for="(t,index) in titles" :key="index">{{ t }}</div>
+           v-for="(t,index) in titles" :key="index"
+           :ref="(el) => { if(el) navItem[index] = el }">{{ t }}
+      </div>
+      <div class="lemon-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="lemon-tabs-content">
       <component class="lemon-tabs-content-item"
@@ -16,6 +19,7 @@
 
 <script lang="ts">
 import Tab from './Tab.vue';
+import {onMounted, onUpdated, ref} from 'vue';
 
 export default {
   props: {
@@ -32,9 +36,25 @@ export default {
     });
     const titles = defaults.map((tag) => { return tag.props.title; });
     const select = (title: String) => {
-      context.emit('update:selected', title)
-    }
-    return {defaults, titles, select};
+      context.emit('update:selected', title);
+    };
+    const navItem = ref<HTMLDivElement[]>([]);  //声明数组是HTMLDiv元素的数组
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    const x = () => {
+      const divs = navItem.value;
+      const result = divs.filter(div => div.classList.contains('selected'))[0];
+      //获取含有selected类名的div元素
+      const {width} = result.getBoundingClientRect();
+      indicator.value.style.width = width + 'px';
+      const {left: left1} = result.getBoundingClientRect();
+      const {left: left2} = container.value.getBoundingClientRect();
+      const left = left1 - left2;
+      indicator.value.style.left = left + 'px';
+    };
+    onMounted(x);
+    onUpdated(x);
+    return {defaults, titles, select, navItem, indicator, container};
   }
 };
 </script>
@@ -48,23 +68,40 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
+
     &-item {
       padding: 8px 0;
       margin: 0 16px;
       cursor: pointer;
+
       &:first-child {
         margin-left: 0;
       }
+
       &.selected {
         color: $blue;
       }
     }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
   }
+
   &-content {
     padding: 8px 0;
+
     &-item {
       display: none;
-      &.selected{
+
+      &.selected {
         display: block;
       }
     }
