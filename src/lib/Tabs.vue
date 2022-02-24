6ce4e6beb98119978c31651822a01942f7bb1d66
zeng715734199 <715734199@qq.com>
2022-02-24 13:54:00 +0800

    使用CSS切换Tab内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>Tabs组件</div>
-  <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-  <component v-for="(c,index) in defaults" :is="c" :key="index"></component>
+  <div class="lemon-tabs">
+    <div class="lemon-tabs-nav">
+      <div class="lemon-tabs-nav-item"
+           :class="{selected: t === selected}"
+           @click="select(t)"
+           v-for="(t,index) in titles" :key="index">{{ t }}</div>
+    </div>
+    <div class="lemon-tabs-content">
+      <component class="lemon-tabs-content-item"
+                 :class="{selected: c.props.title === selected}"
+                 v-for="(c,index) in defaults" :is="c" :key="index"/>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue';
+
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
     const defaults = context.slots.default();
     defaults.forEach((item) => {
@@ -14,8 +30,44 @@ export default {
         throw new Error('Tabs子标签必须是Tab');
       }
     });
-    const titles = defaults.map((tag) =>{ return  tag.props.title })
-    return {defaults, titles};
+    const titles = defaults.map((tag) => { return tag.props.title; });
+    const select = (title: String) => {
+      context.emit('update:selected', title)
+    }
+    return {defaults, titles, select};
   }
 };
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.lemon-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected{
+        display: block;
+      }
+    }
+  }
+}
+</style>
