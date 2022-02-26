e33d7a517d8c25eeee58a90eeb718c908a63ae5c
zeng715734199 <715734199@qq.com>
2022-02-26 19:40:25 +0800

    抽离用户需要拷贝的代码

---
+++ src/components/Switch1.demo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="demo-component">
+    <Switch v-model:value="bool"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue';
+import {ref} from 'vue';
+
+export default {
+  components: {Switch},
+  setup() {
+    const bool = ref(false);
+    return {
+      bool
+    };
+  }
+};
+</script>
