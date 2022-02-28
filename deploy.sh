bba47829271ea07c9030a5844b5cfe61a164f43e
zeng715734199 <715734199@qq.com>
2022-02-28 18:52:48 +0800

    修改错误代码

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m "update" &&
-git branch -M master &&
+git branch -M main &&
 git remote add origin git@github.com:zeng715734199/Lemon-UI.git &&
-git push -f -u origin master &&
+git push -f -u origin main &&
 cd -
 echo https://zeng715734199.github.io/Lemon-UI/#/
