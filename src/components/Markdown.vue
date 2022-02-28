85d673e73bc92bc8666b21bbcfe5862c5a24dc72
zeng715734199 <715734199@qq.com>
2022-02-28 15:08:16 +0800

    静态引入md，忽略dist目录

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -4,21 +4,13 @@
 </template>
 
 <script lang="ts">
-import {ref} from 'vue';
 
 export default {
   props: {
-    path: {
+    content: {
       type: String,
       require: true
     }
-  },
-  setup(props) {
-    const content = ref(null);
-    import(props.path).then(result =>
-        content.value = result.default
-    );
-    return {content}
   }
 };
 </script>
--- src/router.ts
+++ src/router.ts
@@ -5,12 +5,19 @@ import Switch from './components/SwitchDemo.vue';
 import Button from './components/ButtonDemo.vue';
 import Dialog from './components/DialogDemo.vue';
 import Tabs from './components/TabsDemo.vue';
+// @ts-ignore
+import intro from './markdown/intro.md';
+// @ts-ignore
+import install from './markdown/install.md';
+// @ts-ignore
+import getStarted from './markdown/get-started.md';
+
 import Markdown from './components/Markdown.vue';
 import {h} from 'vue';
 
 
 const history = createWebHashHistory();
-const md = fileName => h(Markdown, {path: `../markdown/${fileName}.md`, key: fileName});
+const md = string => h(Markdown, {content: string, key: string});
 export const router = createRouter({
   history: history,
   routes: [
@@ -20,9 +27,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         {path: '', redirect: '/doc/intro'},
-        {path: 'intro', component: md('intro')},
-        {path: 'get-started', component: md('get-started')},
-        {path: 'install', component: md('install')},
+        {path: 'intro', component: md(intro)},
+        {path: 'get-started', component: md(getStarted)},
+        {path: 'install', component: md(install)},
         {path: 'switch', component: Switch},
         {path: 'button', component: Button},
         {path: 'dialog', component: Dialog},
