de59396b03c3c39ca73f08f04faf18695785138e
zeng715734199 <9558145+zeng715734199@user.noreply.gitee.com>
2022-03-02 01:15:15 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M main &&
-git remote add origin git@github.com:zeng715734199/Lemon-sourceCode.git &&
+git remote add origin git@gitee.com:zeng715734199/lemon-source-code.git &&
 git push -f -u origin main &&
 cd -
 echo https://zeng715734199.github.io/Lemon-UI/#/
