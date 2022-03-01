817965d6134944fcc10f62d71b2fce7a16159be7
zeng715734199 <9558145+zeng715734199@user.noreply.gitee.com>
2022-03-02 00:41:20 +0800

    添加README.md

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M main &&
-git remote add origin git@gitee.com:zeng715734199/lemon-ui.git &&
+git remote add origin git@github.com:zeng715734199/Lemon-UI.git &&
 git push -f -u origin main &&
 cd -
 echo https://zeng715734199.github.io/Lemon-UI/#/
